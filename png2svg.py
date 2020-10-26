765f26fa206c47ef553af46fcc28714c70730166
Mocking <fanhaiwang0817@gmail.com>
2020-10-26 17:59:09 +0800

    优化 1.修改Image从PIL获取 2.优化效率

--- png2svg.py
+++ png2svg.py
@@ -10,34 +10,35 @@ from optparse import OptionParser
 
 from PIL import Image
 
-
-
 logging.basicConfig()
 log = logging.getLogger('png2svg')
-
 
 
 def add_tuple(a, b):
     return tuple(map(operator.add, a, b))
 
+
 def sub_tuple(a, b):
     return tuple(map(operator.sub, a, b))
+
 
 def neg_tuple(a):
     return tuple(map(operator.neg, a))
 
+
 def direction(edge):
     return sub_tuple(edge[1], edge[0])
+
 
 def magnitude(a):
     return int(pow(pow(a[0], 2) + pow(a[1], 2), .5))
 
+
 def normalize(a):
     mag = magnitude(a)
     assert mag > 0, "Cannot normalize a zero-length vector"
-    return tuple(map(operator.div, a, [mag]*len(a)))
-    
-                           
+    return tuple(map(operator.div, a, [mag] * len(a)))
+
 
 def svg_header(width, height):
     return """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
@@ -46,6 +47,7 @@ def svg_header(width, height):
 <svg width="%d" height="%d"
      xmlns="http://www.w3.org/2000/svg" version="1.1">
 """ % (width, height)
+
 
 def rgba_image_to_svg_pixels(im, opaque=None):
     s = StringIO()
@@ -58,7 +60,9 @@ def rgba_image_to_svg_pixels(im, opaque=None):
             rgba = im.getpixel(here)
             if opaque and not rgba[3]:
                 continue
-            s.write("""  <rect x="%d" y="%d" width="1" height="1" style="fill:rgb%s; fill-opacity:%.3f; stroke:none;" />\n""" % (x, y, rgba[0:3], float(rgba[3]) / 255))
+            s.write(
+                """  <rect x="%d" y="%d" width="1" height="1" style="fill:rgb%s; fill-opacity:%.3f; stroke:none;" />\n""" % (
+                x, y, rgba[0:3], float(rgba[3]) / 255))
     s.write("""</svg>\n""")
     return s.getvalue()
 
@@ -71,7 +75,7 @@ def joined_edges(assorted_edges, keep_every_point=False):
         (1, 0),
         (0, -1),
         (-1, 0),
-        ])
+    ])
     while assorted_edges:
         if not piece:
             piece.append(assorted_edges.pop())
@@ -101,12 +105,11 @@ def joined_edges(assorted_edges, keep_every_point=False):
 
 
 def rgba_image_to_svg_contiguous(im, opaque=None, keep_every_point=False):
-
     # collect contiguous pixel groups
-    
+
     adjacent = ((1, 0), (0, 1), (-1, 0), (0, -1))
     visited = Image.new("1", im.size, 0)
-    
+
     color_pixel_lists = {}
 
     width, height = im.size
@@ -146,12 +149,12 @@ def rgba_image_to_svg_contiguous(im, opaque=None, keep_every_point=False):
     # calculate clockwise edges of pixel groups
 
     edges = {
-        (-1, 0):((0, 0), (0, 1)),
-        (0, 1):((0, 1), (1, 1)),
-        (1, 0):((1, 1), (1, 0)),
-        (0, -1):((1, 0), (0, 0)),
-        }
-            
+        (-1, 0): ((0, 0), (0, 1)),
+        (0, 1): ((0, 1), (1, 1)),
+        (1, 0): ((1, 1), (1, 0)),
+        (0, -1): ((1, 0), (0, 0)),
+    }
+
     color_edge_lists = {}
 
     for rgba, pieces in color_pixel_lists.items():
@@ -196,13 +199,11 @@ def rgba_image_to_svg_contiguous(im, opaque=None, keep_every_point=False):
                     here = edge[0]
                     s.write(""" L %d,%d """ % here)
                 s.write(""" Z """)
-            s.write(""" " style="fill:rgb%s; fill-opacity:%.3f; stroke:none;" />\n""" % (color[0:3], float(color[3]) / 255))
-            
+            s.write(
+                """ " style="fill:rgb%s; fill-opacity:%.3f; stroke:none;" />\n""" % (color[0:3], float(color[3]) / 255))
+
     s.write("""</svg>\n""")
     return s.getvalue()
-    
-                
-        
 
 
 def png_to_svg(filename, contiguous=None, opaque=None, keep_every_point=None):
@@ -212,28 +213,34 @@ def png_to_svg(filename, contiguous=None, opaque=None, keep_every_point=None):
         sys.stderr.write('%s: Could not open as image file\n' % filename)
         sys.exit(1)
     im_rgba = im.convert('RGBA')
-    
+
     if contiguous:
         return rgba_image_to_svg_contiguous(im_rgba, opaque, keep_every_point)
     else:
         return rgba_image_to_svg_pixels(im_rgba, opaque)
-    
 
 
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("-v", "--verbose", action="store_true", dest="verbosity", help="Print verbose information for debugging", default=None)
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbosity",
+                      help="Print verbose information for debugging", default=None)
     parser.add_option("-q", "--quiet", action="store_false", dest="verbosity", help="Suppress warnings", default=None)
-    parser.add_option("-p", "--pixels", action="store_false", dest="contiguous", help="Generate a separate shape for each pixel; do not group pixels into contiguous areas of the same colour", default=True)
-    parser.add_option("-o", "--opaque", action="store_true", dest="opaque", help="Opaque only; do not create shapes for fully transparent pixels. ", default=None)
-    parser.add_option("-1", "--one", action="store_true", dest="keep_every_point", help="1-pixel-width edges on contiguous shapes; default is to remove intermediate points on straight line edges. ", default=None)
-    
+    parser.add_option("-p", "--pixels", action="store_false", dest="contiguous",
+                      help="Generate a separate shape for each pixel; do not group pixels into contiguous areas of the same colour",
+                      default=True)
+    parser.add_option("-o", "--opaque", action="store_true", dest="opaque",
+                      help="Opaque only; do not create shapes for fully transparent pixels. ", default=None)
+    parser.add_option("-1", "--one", action="store_true", dest="keep_every_point",
+                      help="1-pixel-width edges on contiguous shapes; default is to remove intermediate points on straight line edges. ",
+                      default=None)
+
     (options, args) = parser.parse_args()
-    
+
     if options.verbosity == True:
         log.setLevel(logging.DEBUG)
     elif options.verbosity == False:
         log.setLevel(logging.ERROR)
 
     assert len(args) == 1, "Usage: %s [FILE]"
-    print png_to_svg(args[0], contiguous=options.contiguous, opaque=options.opaque, keep_every_point=options.keep_every_point)
+    print png_to_svg(args[0], contiguous=options.contiguous, opaque=options.opaque,
+                     keep_every_point=options.keep_every_point)
